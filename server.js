fda90844870df2da777709b01592634910ca570c
ZED <ziyobe066@gmail.com>
2024-08-13 13:31:27 +0900

    BRR: Building frontend via EJS framework

--- server.js
+++ server.js
@@ -15,12 +15,20 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Roote code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background: red">Hello World by Ziyoviddin</h1>`);
+// app.get("/hello", function (req, res) {
+//   res.end(`<h1 style="background: red">Hello World by Ziyoviddin</h1>`);
+// });
+
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
+// });
+app.post("/create-item", (req, res) => {
+  console.log(req);
+  res.json({ test: "succss" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
